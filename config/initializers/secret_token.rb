91407ab010d1f12f90d47e86532a50395e35bb5b
Brandon Smith <brandon.smith@yale.edu>
2013-02-12 01:37:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowball::Application.config.secret_token = '837bdd8f2bd881dc6b459d8b8763f8df515da8860ba1627d3d2b760b8a42671e7af0d27d7d19e75ec1b291e138e37e4ab9fb769b4ba3088546c24891a8fd5dae'
