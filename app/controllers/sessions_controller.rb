d616a5096d2826147bedbbb6bd145d75954f8973
Brandon Smith <brandon.smith@yale.edu>
2013-04-11 02:14:33 -0400

    retrieve blogs a user is following

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,13 @@ class SessionsController < ApplicationController
     Tumblr.configure do |config|
 		  config.consumer_key = "zGwB3KqWwxJ1ZFUDxxA6yV9jQmA3aVZR3KatMyFltgg7QaCJyz"
 		  config.consumer_secret = "KyjlTSaMyEVWfzV55DfErmk6v80sSCow4g9SSgYYIkAM3U92j2"
-		  puts "HELLOOOOOOOOOOOOOOOOOO"
 		  config.oauth_token = auth["extra"]["access_token"].token
 		  config.oauth_token_secret = auth["extra"]["access_token"].secret
-		  puts config.oauth_token
-		  puts config.oauth_token_secret
 		end
+
+		client = Tumblr::Client.new
+		puts client.info		
+		puts client.following
 
     redirect_to root_url, :notice => "Signed in!"  
   end  
