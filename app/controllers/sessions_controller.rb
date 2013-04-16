bc990006b5da6f2245edcce2512ff5db55e5a591
Mainak Ghosh <g.mainak@gmail.com>
2013-04-15 22:52:38 -0400

    JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController  
-	require 'open-uri'
+	require "open-uri"
 	def create  
 		auth = request.env["omniauth.auth"]  
 		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)  
@@ -28,20 +28,20 @@ class SessionsController < ApplicationController
 		# than 20, it find 20 blogs at a time and appends the list to 
 		# the blogs variable
 		numFollowing = client.info["user"]["following"]
-		blogs = []
+		blags = []
 		((numFollowing/20.0).ceil).times do |i|
 			x = client.following(:offset => i*20)["blogs"]
-			blogs.concat x
+			blags.concat x
 		end
-		user.following = blogs
+		user.following = blags
 		user.save
 
 		# TODO: Figure out how to get the first 100 posts
-		# right now, we're only grabbing 20 posts. DONE
+		# right now, we"re only grabbing 20 posts. DONE
 		# Figure out how to save the data for each blog so that
 		# some library can read it later for the visualization
 		# TODO: From here (to line 76) should go into a separate thread.
-		f = File.open("blogs.net", 'w+') 
+		f = File.open("blogs.net", "w+") 
 		all_posts = Array.new
 		
 
@@ -50,44 +50,51 @@ class SessionsController < ApplicationController
 		# this would also be a measurement of how influential you are
 		# how many of the people that could possibly be affected by a post are actually
 		# being influenced by a post (at least in terms of likes/reblogs)? 
-		for blog in blogs
+		blogs = ""
+		posts = ""
+		blogs << "{ \n\"blogs\": [\n"
+		posts << "\n\"posts\": [\n"
+		blag = blags[0, 2]
+
+		for blog in blag
 			likes = 0
 			reblogs = 0
 			puts "analyzing: " + blog["name"]
-			f.write(blog["name"] + "\n")
+			blogs << "{ \"blog_name\": \"" + blog["name"] + "\", \"following\": \"true\" },\n"
 			# Each of these should be started in 5 separate processes
 			5.times do |i|
 				sleep 0.5
-				posts = client.posts(blog["name"], :offset => i*20, :reblog_info => true)
-				posts = posts["posts"]
-				for post in posts
-					f.write("\t" + "short_url: " + post["short_url"] + "\n")
-					f.write(
-					(post["reblogged_from_id"].to_s || "") + "\t" + (post["reblogged_from_url"].to_s || "") + "\t" + (post["reblogged_from_name"].to_s || "") + "\t" + (post["reblogged_from_title"].to_s || "") + "\t" + (post["reblogged_root_url"].to_s|| "") + "\t" + (post["reblogged_root_name"].to_s || "") + "\t" + (post["reblogged_root_title"].to_s || "") + "\n" )
-					
-					arr = ""
+				posts_array = client.posts(blog["name"], :offset => i*20, :reblog_info => true, :limit => 5)
+				posts_array = posts_array["posts"]
+				for post in posts_array
+					# posts << "short_url: " + post["short_url"] + ",\n"
+					posts << "{ \"reblogging\": \""+ blog["name"] + "\", \"source\": \"" + (post["reblogged_from_name"].to_s || "") + "\"},\n"
+					if !post["reblogged_from_name"].nil?
+						blogs << "{ \"blog_name\": \"" + post["reblogged_from_name"].to_s + "\", \"following\": \"false\"},\n"
+					end
 					doc = Nokogiri::HTML(open(post["short_url"]))
-					doc.css('ol.notes').each do |node|
-						node.css('li.like').each do |note|
+					doc.css("ol.notes").each do |node|
+						node.css("li.like").each do |note|
 							likes+=1
 						end
-						node.css('li.reblog').each do |note|
+						node.css("li.reblog").each do |note|
 							reblogging = note.at_css("span .tumblelog")
 							source = note.at_css("span .source_tumblelog")
 							if(source && reblogging)
 								# puts "#{reblogging} reblogged from #{source}"
-								arr << ("\t" + reblogging + " " + 
-									source + "\n")
+								posts << "{ \"reblogging\": \"" + reblogging + "\", \"source\": \"" + source + "\"},\n"
 							end
 							reblogs+=1
 						end
 					end
-					f.write(arr)
 					puts post["short_url"]
 				end
 			end
 			#puts " #{blog["name"]} has #{likes} likes and #{reblogs} reblogs"
 		end
+		blogs << "],"
+		posts << "]}"
+		f.write(blogs + posts)
 		f.close
 	
     redirect_to root_url, :notice => "Signed in!"  
